ad413da2f957e1eb2c61731930a26b2bbc766c26
subeyugi <164650867+subeyugi@users.noreply.github.com>
2024-09-28 20:55:37 +0900

    Update submit.js

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,8 +1,8 @@
 export async function onRequest(context) {
-  try{
-    console.log("onRequest send");
-    return new Response("sent!!");
-  }catch(err){
-    return new Response("error", {status:400});
-  }
+    try{
+      console.log("onRequest send");
+      return new Response("sent!!");
+    }catch(err){
+      return new Response("error", {status:400});
+    }
 }
